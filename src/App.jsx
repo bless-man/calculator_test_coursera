eb7abc2590d62fc036c430c585fc3f6970d0111b
Serhii Tustanovskyi <serhii@MacBook-Air-Serhii.local>
2025-02-02 15:54:49 +0100

    feat:create calculator

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Calculator } from './components/Calculator'
+
+function App() {
+
+  return (
+    <>
+     <h1>Simplest Working Calculator</h1>
+      <Calculator/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+
+import "./calculator.css";
+
+export class Calculator extends Component {
+  btns = [
+    {
+      id: "1",
+      title: "add",
+      value: "+",
+    },
+    {
+      id: "2",
+      title: "substract",
+      value: "-",
+    },
+    {
+      id: "3",
+      title: "multiply",
+      value: "*",
+    },
+    {
+      id: "4",
+      title: "divide",
+      value: "/",
+    },
+  ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      result: "",
+    };
+  }
+
+  handleClick = (value) => {
+    this.setState((prevState) => ({ input: prevState.input + value }));
+  };
+  clearInput = () => {
+    this.setState({ input: "" });
+  };
+  clearResult = () => {
+    this.setState({ result: "" });
+  };
+  handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      this.calculateResult();
+    }
+  };
+  calculateResult = () => {
+    try {
+      this.setState({ result: eval(this.state.input).toString() });
+      this.setState({ input: '' });
+    } catch (error) {
+      this.setState({ result: "Error" });
+    }
+  };
+
+  render() {
+    return (
+      <div className="calculator">
+        <div className="calculator__inputs">
+          <label>Result:</label>
+          <input
+            type="text"
+            value={this.state.result}
+            readOnly
+            className="calculator__display"
+          />
+          <label>Calculator:</label>
+          <input
+            type="text"
+            value={this.state.input}
+            onChange={(e) => this.setState({ input: e.target.value })}
+            onKeyDown={this.handleKeyPress}
+            className="calculator__input"
+          />
+        </div>
+        <div className="calculator__actions">
+          {this.btns.map(({ title, value, id }) => (
+            <button
+              key={id}
+              onClick={() => this.handleClick(value)}
+              className="calculator__button"
+            >
+              {title}
+            </button>
+          ))}
+          <button
+            onClick={this.clearInput}
+            className="calculator__button calculator__clear"
+          >
+            Clear input
+          </button>
+          <button
+            onClick={this.clearResult}
+            className="calculator__button calculator__clear"
+          >
+            Clear result
+          </button>
+          <button
+            onClick={this.calculateResult}
+            className="calculator__button calculator__equal"
+          >
+            Get Result
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
